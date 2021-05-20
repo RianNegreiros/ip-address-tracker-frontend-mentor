9a2179f8f1986c2cf8d8160ce4e90380120b1906
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-20 01:21:26 -0300

    feat: dinamic informations

--- src/components/informations/informations.jsx
+++ src/components/informations/informations.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import context from '../../context/context'
 import './informations-styles.scss'
 
 const Informations = () => {
+    const { state } = useContext(context)
     return (
         <section>
             <article className="ip-address">
                 <p>IP ADDRESS</p>
-                <h2>192.212.174.101</h2>
+                <h2>{state.ip}</h2>
             </article>
 
             <article className="location">
                 <p>LOCATION</p>
-                <h2>Brooklyn, NY 1001</h2>
+                <h2>{`${state.city}, ${state.region}, ${state.postalCode}`}</h2>
             </article>
 
             <article className="timezone">
                 <p>TIMEZONE</p>
-                <h2>UTC -05:00</h2>
+                <h2>{state.timezone}</h2>
             </article>
 
             <article className="isp">
                 <p>ISP</p>
-                <h2>SpaceX Starlink</h2>
+                <h2>{state.isp}</h2>
             </article>
         </section>
     )
