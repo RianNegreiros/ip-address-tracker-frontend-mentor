4aac5eaa8e360f1a5dc44729e980a629dead9c7d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-20 01:22:11 -0300

    refactor: states refactor

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { useState } from 'react'
 import Map from './components/map/map'
 import Header from './components/header/header'
 import Informations from './components/informations/informations'
+import Context from './context/context'
 
 const App = () => {
-  const [inputText, setInputText] = useState<String>('')
-
+  const [state, setState] = useState({
+    ip: '8.8.8.8',
+    lat: 37.38605,
+    lng: -122.08385,
+    city: 'Mountain View',
+    region: 'California',
+    postalCode: '94043',
+    timezone: 'UTC -07:00',
+    isp: 'Google LLC',
+  })
+  
 
   return (
-    <>
-    <Header inputText={inputText} setInputText={setInputText} />
+    <Context.Provider value={{ state, setState}}>
+    <Header />
     <Informations />
     <Map />
-    </>
+    </Context.Provider>
   )
 }
 
