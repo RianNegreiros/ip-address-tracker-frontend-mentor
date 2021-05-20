50c9d5e38acaaa0c18e9c994120b603e3d0c1de9
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2021-05-20 01:24:49 -0300

    feat: new map using react-google-map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,26 +1,24 @@
-import axios from 'axios'
-import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useContext } from 'react'
+import GoogleMapReact from 'google-map-react'
+import context from '../../context/context'
+import Marker from '../marker/marker'
 import './map-styles.scss'
 
 const Map = () => {
-  const apiUrl = 'https://geo.ipify.org/api/v1?apiKey=at_WUJobuHlWQRt7209TiDp5zHsOO9GP&ipAddress=8.8.8.8'
-  axios.get(apiUrl).then((res) => res.data)
-  
-
+  const { state } = useContext(context)
 
   return (
-    <MapContainer center={[37.38605, -122.08385]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBn_3ejZnHXRfR6XRgnmIDjGW5nv3lOvr0' }}
+        defaultCenter={[state.lat, state.lng]}
+        defaultZoom={15}
+        center={[state.lat, state.lng]}
+      >
+        <Marker lat={state.lat} lng={state.lng}  />
+      </GoogleMapReact>
+    </div>
+
   )
 }
 
