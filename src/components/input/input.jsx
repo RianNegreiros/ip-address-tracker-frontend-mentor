45733fc6e0f80ca6f2c8f82b65e287502374ceec
Rian Negreiros <riannegreiros@gmail.com>
2021-05-20 16:50:22 -0300

    fix: button functionality

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -37,7 +37,7 @@ const Input = () => {
                     pattern="^(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$"
                 />
             </form>
-            <button></button>
+            <button onClick={handleSubmit}></button>
         </div>
     )
 }
